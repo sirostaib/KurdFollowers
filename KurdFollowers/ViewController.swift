cc13ac36fd5c64cbd237d0143850ca4104beea36
Caesar <sirostaib@gmail.com>
2024-04-19 15:08:04 +0300

    initial commit
    added empty swift UIKit project

---
+++ KurdFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KurdFollowers
+//
+//  Created by Siros Taib on 4/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
