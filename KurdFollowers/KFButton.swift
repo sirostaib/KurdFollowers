603e4e4e83215c112ea03626f4406279e33bd73f
Caesar <sirostaib@gmail.com>
2024-04-21 15:03:18 +0300

    feat: :sparkles: added custom button

---
+++ KurdFollowers/KFButton.swift
@@ -0,0 +1,34 @@
+//
+//  KFButton.swift
+//  KurdFollowers
+//
+//  Created by Siros Taib on 4/21/24.
+//
+
+import UIKit
+
+class KFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(bgColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = bgColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
