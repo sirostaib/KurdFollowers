812f8d0904584f6710e7035c4c36626fb094e0df
Caesar <sirostaib@gmail.com>
2024-04-21 16:37:08 +0300

    feat: :sparkles: added the models
    follower and user

---
+++ KurdFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  KurdFollowers
+//
+//  Created by Siros Taib on 4/21/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
